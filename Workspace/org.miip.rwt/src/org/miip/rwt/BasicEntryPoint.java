fdb9e3e599c3b211551cb1a4f91b69ea84ec46f0
Kees <info@condast.com>
2017-12-24 16:45:57 +0100

    Window designer improved

--- Workspace/org.miip.rwt/src/org/miip/rwt/BasicEntryPoint.java
+++ Workspace/org.miip.rwt/src/org/miip/rwt/BasicEntryPoint.java
@@ -1,25 +1,40 @@
 package org.miip.rwt;
 
+import org.condast.commons.xml.BuildEvent;
+import org.condast.commons.xml.IBuildListener;
 import org.eclipse.rap.rwt.application.AbstractEntryPoint;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.miip.rwt.frontend.Frontend;
+import org.eclipse.swt.widgets.Widget;
 import org.miip.rwt.service.Dispatcher;
+import org.miip.waterway.ui.swt.MiipComposite;
 import org.miip.waterway.ui.xml.XMLFactoryBuilder;
 
 public class BasicEntryPoint extends AbstractEntryPoint {
 	private static final long serialVersionUID = 1L;
 
+	private IBuildListener<Widget> listener = new IBuildListener<Widget>(){
+
+		@Override
+		public void notifyTestEvent(BuildEvent<Widget> event) {
+			if( event.getData() instanceof MiipComposite){
+				MiipComposite miipcomp = (MiipComposite) event.getData();
+		        Dispatcher.getInstance().startApplication( miipcomp );
+			}
+		}
+		
+	};
+	
 	@Override
     protected void createContents(Composite parent) {
-        parent.setLayout(new GridLayout(1, false));
+        parent.setLayout(new FillLayout());
         //Frontend frontend = new Frontend( parent, SWT.NONE );
         //frontend.setLayoutData( new GridData( SWT.FILL, SWT.FILL, true, true ));
         
         XMLFactoryBuilder builder = new XMLFactoryBuilder( parent, this.getClass());
+        builder.addListener(listener);
         builder.build();
-        Dispatcher.getInstance().startApplication( (Frontend) builder.getRoot());
-    }
+        builder.removeListener(listener);
+   }
 }
