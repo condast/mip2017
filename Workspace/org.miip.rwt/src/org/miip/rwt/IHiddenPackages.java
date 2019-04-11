398e6cd0294667ade8ab31ec67b587167756b5e7
Kees Pieters <info@condast.com>
2019-04-11 13:00:24 +0200

    Added hidden imports

--- Workspace/org.miip.rwt/src/org/miip/rwt/IHiddenPackages.java
+++ Workspace/org.miip.rwt/src/org/miip/rwt/IHiddenPackages.java
@@ -4,6 +4,10 @@ package org.miip.rwt;
 
 import org.condast.commons.ui.image.*;
 import org.condast.commons.ui.logger.*;
+import org.condast.commons.ui.banner.*;
+
+import org.eclipse.jface.window.*;
+
 import org.miip.waterway.model.eco.MIIPEnvironment;
 import org.miip.waterway.ui.swt.*;
 import org.miip.waterway.ui.swt.pond.*;
