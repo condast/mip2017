a6b7e87775445441bba1bbf73c8d23d89a5ac05f
Kees Pieters <info@condast.com>
2022-04-07 22:55:49 +0200

    Improved testing

--- Workspace/test.org.condast.rcp/src/test/org/condast/rcp/BasicApplication.java
+++ Workspace/test.org.condast.rcp/src/test/org/condast/rcp/BasicApplication.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.condast.commons.ui.entry.EntryFactoryBuilder;
 import org.eclipse.rap.rwt.application.Application;
 import org.eclipse.rap.rwt.application.ApplicationConfiguration;
+import org.eclipse.rap.rwt.application.Application.OperationMode;
 import org.eclipse.rap.rwt.client.WebClient;
 
 import test.org.condast.rcp.entries.BasicEntryPoint;
@@ -13,9 +14,15 @@ import test.org.condast.rcp.entries.FieldDesignerEntryPoint;
 
 public class BasicApplication implements ApplicationConfiguration {
 
+	public static final String S_ARNAC = "/arnac";
+	private static final String S_ARNAC_THEME = "arnac.theme";
+	private static final String S_THEME_CSS = "themes/theme.css";
+
     public void configure(Application application) {
         Map<String, String> properties = new HashMap<String, String>();
         properties.put(WebClient.PAGE_TITLE, "Hello RAP");
+		application.addStyleSheet( S_ARNAC_THEME, S_THEME_CSS );
+		application.setOperationMode( OperationMode.SWT_COMPATIBILITY );
         application.addEntryPoint("/hello", BasicEntryPoint.class, properties);
         application.addEntryPoint("/testfield", FieldDesignerEntryPoint.class, properties);
         
